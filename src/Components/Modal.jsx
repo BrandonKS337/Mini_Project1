ee4f7f7b93b82fb75f862b34b0644b7327521e2b
Project V2 <brandonks337@gmail.com>
2023-07-26 18:14:37 -0500

    bare bones design of sign up overlay in place. Needs some more styling love but its there

--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -1,5 +1,10 @@
+// import { title } from "process";
 import React from "react";
 import ReactDOM from "react-dom";
+import appleButton from "../Assets/buttons/appleButton.svg";
+import googleButton from "../Assets/buttons/googleButton.svg";
+import facebookButton from "../Assets/buttons/facebookButton.svg";
+
 
 const MODAL_STYLES = {
   position: "fixed",
@@ -24,6 +29,45 @@ const OVERLAY_STYLES = {
   zIndex: 1000,
 };
 
+const Overlay = {
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  margin: "10px",
+  gap: "15px",
+};
+
+const title = {};
+
+const closeModal = {
+  width: "35%",
+  borderRadius: "20px",
+  position: "fixed",
+  bottom: "15px",
+  gap: "15px",
+};
+
+const loginButtons = {
+  display: "flex",
+  flexDirection: "row",
+  gap: "15px",
+  
+
+};
+
+const svgButtonStyle = {
+  background: "none",
+  border: "none",
+  // width: '100%',
+  alignItems: "center",
+};
+const altLoginButtons = {
+  display: "flex",
+  flexDirection: "column",
+  width: "100%",
+};
+
 export default function Modal({ open, children, onClose }) {
   if (!open) return null;
 
@@ -31,30 +75,53 @@ export default function Modal({ open, children, onClose }) {
     <>
       <div style={OVERLAY_STYLES} />
       <div style={MODAL_STYLES}>
-        <div>
-          <div>Create Account</div>
+        <div style={Overlay}>
+          <div style={title}>
+            <h2>Create Account</h2>
+          </div>
           <div>
-            <div>User Name</div>
+            <input type="userName" placeholder="UserName..." id="userName" />
             {/* Input field for User Name */}
           </div>
           <div>
-            <div>Email</div>
-            {/* Input field for Email */}
+            <input type="userEmail" placeholder="Email..." id="userEmail" />
+            {/* Input field for User Name */}
           </div>
           <div>
-            <div>Password</div>
-            {/* Input field for Password */}
+            <input
+              type="userPassword"
+              placeholder="Password..."
+              id="userPassword"
+            />
+            {/* Input field for User Name */}
           </div>
-          <div>LOGIN</div>
-          <div>Sign In</div>
-          <div>Sign in with Google</div>
-          <div>Log in with Google</div>
-          <div>Continue with Apple</div>
-          <div>OR</div>
+          <div style={loginButtons} type="login/signin buttons">
+            <button>Sign Up</button>
+            <button>Sign In</button>
+          </div>
+          <div><img src={require("../Assets/images/Or Divider Red.svg").default} alt="Divider Red" /></div>
+          <div style={altLoginButtons}>
+              <button style={svgButtonStyle}>
+                <a href="https://www.facebook.com">
+                  <img src={facebookButton} />
+                </a>
+              </button>
+              <button style={svgButtonStyle}>
+                <a href="https://www.google.com">
+                  <img src={googleButton} />
+                </a>
+              </button>
+              <button style={svgButtonStyle}>
+                <a href="https://www.apple.com">
+                  <img src={appleButton} />
+                </a>
+              </button>
+            </div>
+          <button style={closeModal} onClick={onClose}>
+            Close Modal
+          </button>
+          {children}
         </div>
-
-        <button onClick={onClose}>Close Modal</button>
-        {children}
       </div>
     </>,
     document.getElementById("portal")
